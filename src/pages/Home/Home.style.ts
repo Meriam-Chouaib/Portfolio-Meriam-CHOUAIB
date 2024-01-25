2d55cde9d1440341c6e1e587bb5178305ba33e18
Meriam-Chouaib <mchouaib@anypli.com>
2024-01-25 09:54:02 +0100

    LB-8:Fixing the swiper style

---
+++ src/pages/Home/Home.style.ts
@@ -0,0 +1,7 @@
+import { Box, styled } from '@mui/material'
+
+export const BoxStyled = styled(Box)({
+  '.swiper-3d .swiper-slide-shadow': {
+    backgroundColor: '#007aff00 !important',
+  },
+})
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,9 @@ import { StepsRecord } from 'pages/SignupPage/SignupPage.type'
 import CustomImage from 'components/Common/Image/Image'
 
 import { HEIGHT_SWIPER, SWIPER_ITEMS } from 'pages/Home/Home.constants'
-import { Stack } from '@mui/system'
 import { EffectCards } from 'swiper/modules'
 
-import { Box } from '@mui/material'
-import CustomSwiperSlide from 'components/CustomSwiper/CustomSwiper'
+import { BoxStyled } from 'pages/Home/Home.style'
 interface HomeProps {
   oldStepsRecord: StepsRecord
   onNextStep: (newStepsRecord: StepsRecord) => void
@@ -19,12 +17,16 @@ function Home() {
   const { t } = useTranslation()
 
   return (
-    <Box margin={'2rem 0rem'}>
+    <BoxStyled margin={'2rem 0rem'}>
       <Swiper
         effect={'cards'}
         grabCursor={true}
         modules={[EffectCards]}
-        className='mySwiper'
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
         {SWIPER_ITEMS.map((swiper, index) => (
           <SwiperSlide>
@@ -33,12 +35,12 @@ function Home() {
               height={HEIGHT_SWIPER}
               width={'50%'}
               key={`swiper-library-${index}`}
+              sx={{ borderRadius: '20px' }}
             />
           </SwiperSlide>
         ))}
-        ...
       </Swiper>
-    </Box>
+    </BoxStyled>
   )
 }
 
