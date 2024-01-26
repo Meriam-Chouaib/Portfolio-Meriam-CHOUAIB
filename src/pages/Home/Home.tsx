7835dc61d820ea1764b6d2e34aa67d359c69b6cd
Meriam-Chouaib <mchouaib@anypli.com>
2024-01-26 14:15:36 +0100

    LB-8: Fixing swiper animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,47 @@
 import { useTranslation } from 'react-i18next'
-import { SWIPER_ITEMS } from 'pages/Home/Home.constants'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-import { BoxStyled } from 'pages/Home/Home.style'
+import { StepsRecord } from 'pages/SignupPage/SignupPage.type'
+import CustomImage from 'components/Common/Image/Image'
+
+import { HEIGHT_SWIPER, SWIPER_ITEMS } from 'pages/Home/Home.constants'
+import { EffectCards } from 'swiper/modules'
 import { Box, Typography } from '@mui/material'
+import { BoxStyled } from 'pages/Home/Home.style'
 import BoxCenter from 'components/Common/BoxCenter/BoxCenter'
-import CustomSwiper from 'components/CustomSwiper/CustomSwiper'
-
+interface HomeProps {
+  oldStepsRecord: StepsRecord
+  onNextStep: (newStepsRecord: StepsRecord) => void
+  onPreviousStep: () => void
+}
 function Home() {
   const { t } = useTranslation()
 
   return (
     <BoxCenter>
       <BoxStyled margin={'2rem 0rem'}>
-        <CustomSwiper SWIPER_ITEMS={SWIPER_ITEMS} />
+        <Swiper
+          effect={'cards'}
+          grabCursor={true}
+          modules={[EffectCards]}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {SWIPER_ITEMS.map((swiper, index) => (
+            <SwiperSlide>
+              <CustomImage
+                src={swiper.img}
+                height={HEIGHT_SWIPER}
+                width={'50%'}
+                key={`swiper-library-${index}`}
+                sx={{ borderRadius: '20px' }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
         <Box>
           <Typography variant='h1'>{t('home.name')}</Typography>
         </Box>
@@ -20,5 +49,4 @@ function Home() {
     </BoxCenter>
   )
 }
-
 export default Home
